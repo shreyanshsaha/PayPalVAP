b2831b80b580cdf609ead21181f19cb38e6140d5
Shreyansh Saha <yanshrey@gmail.com>
2020-03-31 14:33:04 +0530

    Changed function in fileHandling.js to async/await

--- courseRegistration/fileHandling.js
+++ courseRegistration/fileHandling.js
@@ -18,148 +18,173 @@ const appendFile = util.promisify(fs.appendFile);
 const readFile = util.promisify(fs.readFile);
 const renameFile = util.promisify(fs.rename);
 
-function searchFile(filename, value, property = 'username', returnOne = true) {
+async function getDataAsArray(filename) {
   return new Promise((resolve, reject) => {
-    getDataAsArray(filename)
-      .then((dataframe) => {
-        let objectDetails = _.filter(dataframe, (row) => {
-          return row[property] == value;
-        });
-        // console.log(userDetails);
-        if (objectDetails && returnOne)
-          objectDetails = objectDetails[0];
+    try {
+      let data = await readFile(filename, 'utf-8');
+      let lines = data.split("\n");
 
-        resolve(objectDetails);
-      })
-      .catch((err) => reject(new Error(err)));
+      // Remove empty lines if any
+      lines = _.filter(lines, (element) => {
+        return element.length > 0;
+      });
 
+      // Parse all elements to json
+      let dataframe = _.map(lines, (element) => {
+        return JSON.parse(element);
+      });
+
+      resolve(dataframe);
+    }
+    catch (err) {
+      reject(err);
+    }
   });
 }
 
-function addToFile(filename, userData, primaryKey = "_id") {
+async function searchFile(filename, value, property = 'username', returnOne = true) {
+  return new Promise((resolve, reject) => {
+    try {
+      let dataframe = await getDataAsArray(filename);
+
+      let objectDetails = _.filter(dataframe, (row) => {
+        return row[property] == value;
+      });
+
+      if (objectDetails && returnOne)
+        objectDetails = objectDetails[0];
+
+      resolve(objectDetails);
+    }
+    catch (err) {
+      reject(err);
+    }
+  });
+}
+
+async function addToFile(filename, userData, primaryKey = "_id") {
   return new Promise((resolve, reject) => {
     if (!(typeof userData == "string"))
       return reject(new Error("Invalid Data, String expected"));
     if (!(typeof primaryKey == "string"))
       return reject(new Error("Invalid key, String expected"));
 
-    readFile(filename, 'utf-8')
-      .then((data) => {
-        let tempUserData = JSON.parse(userData);
-        tempUserData.createdOn = Date.now();
-        let _id = crypto.createHash('md5').update(userData).digest("hex");
-        tempUserData._id = _id;
-        let lines = data.split("\n");
-        for (let i = 0; i < lines.length; i++) {
-          if (lines[i].length <= 0)
-            continue;
-          if (JSON.parse(lines[i])[primaryKey] == tempUserData[primaryKey]) {
-            console.log(JSON.parse(lines[i]), tempUserData)
-            throw new Error(primaryKey + " already exists");
-          }
+    try {
+      let tempUserData = JSON.parse(userData);
+      let _id = crypto.createHash('md5').update(userData).digest("hex");
+
+      tempUserData.createdOn = Date.now();
+      tempUserData._id = _id;
+
+      // Checking if user already exists
+      // TODO: Optimize this
+      let data = readFile(filename, 'utf-8');
+      let lines = data.split("\n");
+      for (let i = 0; i < lines.length; i++) {
+        if (lines[i].length <= 0)
+          continue;
+        if (JSON.parse(lines[i])[primaryKey] == tempUserData[primaryKey]) {
+          return reject(new Error(primaryKey + " already exists"));
         }
-        return JSON.stringify(tempUserData);
-      })
-      .then(async (userData) => {
-        await appendFile(filename, userData + "\n")
-        resolve(JSON.parse(userData));
-      })
-      .catch((err) => reject(err));
+      }
 
+      await appendFile(filename, tempUserData + "\n")
+      resolve(JSON.parse(tempUserData));
+    }
+    catch (err) {
+      reject(err);
+    }
   });
-
 }
 
-function updateData(filename, newData, checkProperty = '_id') {
+async function updateData(filename, newData, checkProperty = '_id') {
   return new Promise((resolve, reject) => {
+
     if (!(typeof newData == "string"))
       return reject(new Error("Invalid Data, String expected"));
+
     let tempFileName = filename + ".temp";
-    readFile(filename, 'utf-8')
-      .then((data) => {
-        let writeFileStream = fs.createWriteStream(tempFileName);
-        if (!writeFileStream)
-          throw new Error("Error creating temp file");
-        let tempData = JSON.parse(newData);
-        let lines = data.split("\n");
-        for (let i = 0; i < lines.length; i++) {
-          if (lines[i].length <= 0)
-            continue;
-          if (JSON.parse(lines[i])[checkProperty] != tempData[checkProperty])
-            writeFileStream.write(lines[i] + "\n");
-          else
-            writeFileStream.write(newData + "\n");
-        }
-        writeFileStream.end();
-      })
-      .then(() => resolve(renameFile(tempFileName, filename)))
-      .then(infoPrint("Updated: " + JSON.parse(newData)[checkProperty] + " in " + filename))
-      .then(() => resolve(newData))
-      .catch((err) => reject(new Error(err)));
+
+    try {
+
+      let data = await readFile(filename, 'utf-8')
+
+      let writeFileStream = fs.createWriteStream(tempFileName);
+      if (!writeFileStream)
+        return reject(new Error("Error creating temp file"));
+
+      let tempData = JSON.parse(newData);
+      let lines = data.split("\n");
+
+      // Updating files by rewriting
+      for (let i = 0; i < lines.length; i++) {
+        if (lines[i].length <= 0)
+          continue;
+        if (JSON.parse(lines[i])[checkProperty] != tempData[checkProperty])
+          writeFileStream.write(lines[i] + "\n");
+        else
+          writeFileStream.write(newData + "\n");
+      }
+
+      writeFileStream.end();
+
+
+      renameFile(tempFileName, filename)
+        .then(() => {
+          infoPrint("Updated: " + JSON.parse(newData)[checkProperty] + " in " + filename);
+          return resolve(newData);
+        })
+        .catch((err) => reject(new Error(err)));
+    }
+    catch (err) {
+      reject(err);
+    }
+
+
   });
 }
 
-function deleteData(filename, dataToDelete, checkProperty = '_id') {
+async function deleteData(filename, dataToDelete, checkProperty = '_id') {
   return new Promise((resolve, reject) => {
     if (!(typeof dataToDelete == "string"))
       return reject(new Error("Invalid Data, String expected"));
 
     let tempFileName = filename + ".temp";
 
-    readFile(filename, 'utf-8')
-      .then((data) => {
-        let writeFileStream = fs.createWriteStream(tempFileName);
-        if (!writeFileStream)
-          throw new Error("Error creating temp file");
+    try {
 
-        let tempData = JSON.parse(dataToDelete);
-        if (!tempData._id)
-          throw new Error("_id required to delete");
-        let lines = data.split("\n");
-        for (let i = 0; i < lines.length; i++) {
-          if (lines[i].length <= 0)
-            continue;
-          // console.log(lines);
-          // console.log(lines[i], tempData[checkProperty]);
-          if (JSON.parse(lines[i])[checkProperty] != tempData[checkProperty]) {
-            // console.log(lines[i], tempData[checkProperty]);
-            writeFileStream.write(lines[i] + "\n");
-          }
-        }
-        writeFileStream.end();
-      })
-      .then(() => {
-        infoPrint(dataToDelete + " deleted from file " + filename);
-        renameFile(tempFileName, filename);
-      })
-      .catch((err) => reject(new Error(err)))
-      .finally(() => resolve(dataToDelete));
+      let data = await readFile(filename, 'utf-8')
 
-  });
-}
+      let writeFileStream = fs.createWriteStream(tempFileName);
+      if (!writeFileStream)
+        return reject(new Error("Error creating temp file"));
 
-function getDataAsArray(filename) {
-  // TODO: Add only if username doesnt already exists
-  return new Promise((resolve, reject) => {
-    readFile(filename, 'utf-8')
-      .then((data) => {
-        let lines = data.split("\n");
+      let tempData = JSON.parse(dataToDelete);
+      if (!tempData._id)
+        return reject(new Error("_id required to delete"));
 
-        // Remove empty lines if any
-        lines = _.filter(lines, (element) => {
-          return element.length > 0;
-        });
+      let lines = data.split("\n");
 
-        // Parse all elements to json
-        let dataframe = _.map(lines, (element) => {
-          return JSON.parse(element);
-        });
+      // Delete file by rewriting
+      for (let i = 0; i < lines.length; i++) {
+        if (lines[i].length <= 0)
+          continue;
+        if (JSON.parse(lines[i])[checkProperty] != tempData[checkProperty])
+          writeFileStream.write(lines[i] + "\n");
+      }
 
-        resolve(dataframe);
-      })
-      .catch((err) => {
-        reject(new Error(err));
-      });
+      writeFileStream.end();
+
+      infoPrint(dataToDelete + " deleted from file " + filename);
+
+      renameFile(tempFileName, filename)
+        .then(() => resolve(dataToDelete))
+        .catch((err) => reject(new Error(err)));
+    }
+    catch (err) {
+      reject(err);
+    }
+
   });
 }
 
